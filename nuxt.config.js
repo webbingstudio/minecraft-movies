2020784591eaecd48c2feafb6fa73aec2511731d
Seiko Kuchida <mail@webbingstudio.com>
2021-07-19 23:18:18 +0900

    echo.cssを読み込む

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,16 +6,20 @@ export default {
   head: {
     title: 'minecraft-movies',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/echo.min.css' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' }
     ]
   },
 
