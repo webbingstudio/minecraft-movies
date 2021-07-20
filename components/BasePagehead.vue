3922b0df014088f80280a202cce7a05865b83058
Seiko Kuchida <mail@webbingstudio.com>
2021-07-20 13:22:19 +0900

    ページタイトルのコンポーネントを作成

---
+++ components/BasePagehead.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="echo-p-pagehead echo-margin-bottom-lg echo-p-pagehead-displayed echo-p-pagehead-darken echo-p-pagehead-blended">
+    <div class="echo-hero echo-hero-fit echo-hero-inverse">
+        <div class="echo-hero-image">
+                        <picture>
+                            <source
+                    media="(max-width:640px)"
+                    srcset="https://picsum.photos/480/320 640w"
+                    sizes="100vw">
+                <source
+                    media="(max-width:960px)"
+                    srcset="https://picsum.photos/960/640 960w"
+                    sizes="100vw">
+                <img src="https://picsum.photos/960/640" alt="">
+                        </picture>
+                    </div><!-- /.echo-hero-image -->
+        <div class="echo-hero-contents">
+            <div class="echo-hero-contents-inner">
+                <div class="echo-hero-header">
+                    <h1 class="echo-p-pagehead-title echo-hero-title echo-title echo-title-style-a echo-title-level-5">本文には画像がありませんがアイキャッチ画像を指定しているエントリーです</h1>
+                </div><!-- /.echo-hero-header -->
+                <div class="echo-hero-body">
+                    <time class="echo-p-pagehead-meta" datetime="2021-01-20T09:24:00+09:00">2021.01.20</time>
+                    <p class="echo-p-pagehead-category">
+                        <a href="#" class="echo-post-category-label echo-label echo-label-style-a">カテゴリー</a>
+                    </p>
+                </div><!-- /.echo-hero-body -->
+            </div><!-- /.echo-hero-contents-inner -->
+        </div><!-- /.echo-hero-contents -->
+    </div><!-- /.echo-hero -->
+</div><!-- /.echo-p-pagehead -->
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
