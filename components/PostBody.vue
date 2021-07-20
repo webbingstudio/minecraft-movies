12faf6dcde324b9fb1e2e12b5efad750b0118445
Seiko Kuchida <mail@webbingstudio.com>
2021-07-20 13:22:58 +0900

    記事本文のコンポーネントを作成

---
+++ components/PostBody.vue
@@ -0,0 +1,31 @@
+<template>
+<section class="echo-p-post echo-container echo-container-sm echo-margin-y-lg">
+
+    <div class="echo-p-post-contents echo-body echo-body-fixed echo-text-sm">
+        <p>デラは急に、窓のほうから向きを変え、鏡の前に立ちました。その眼はきらきら輝いていましたが、顔は二〇秒間まっ青でした。彼女は手早く髪をほどいて下ろし、まっすぐ垂らしました。<br /><br />ジェームズ・デリンガム・ヤング家には、大いに誇れる持ち物が二つありました。<br />一つは、ジムの金の懐中時計。おじいさんから代々受け継がれたものです。<br />もう一つは、デラの髪の毛でした。<br />もしもシバの女王がお隣に住んでいたとして、ある日デラが髪を乾かすために窓から外に垂らしたら、それだけで女王陛下の宝石も財宝も価値がなくなってしまうでしょう。もしもソロモン王がアパートの管理人で、宝物を地下室に詰め込んであったとしても、ジムがそこを通りかかって時計を出すのを見たら、王様は自分のひげを引っ張って悔しがるでしょう。<br /><br />さて、デラの美しい髪は、栗色の滝のように輝き、波のようにうねって、彼女のまわりに垂れていました。それはデラのひざまで届く長さで、まるで着物のようでした。<br />デラは少しいらいらした様子で、すばやく髪をまとめました。<br />そしていったん動きを止め、一分間、ためらっていましたが、やがて一滴、二滴と涙がこぼれ、すり切れた赤いカーペットを濡らしました。<br /><br />それからデラは、くたびれた茶色のジャケットを着て、くたびれた茶色の帽子をかぶりました。スカートをひるがえし、眼には涙の鮮やかなきらめきを浮かべたまま、ドアから出て、通りへと階段を降りました。</p>
+    </div><!-- /.echo-p-post-contents -->
+
+    <div class="echo-p-post-footer">
+        <div class="echo-p-post-tag echo-p-post-tag-all">
+            <nav class="echo-labels">
+                <dl class="echo-labels-list">
+                    <dt class="echo-labels-header">Tags</dt>
+                    <dd class="echo-labels-item"><a href="#" class="echo-p-post-tag-label echo-label echo-label-style-a">たぐ</a></dd>
+                    <dd class="echo-labels-item"><a href="#" class="echo-p-post-tag-label echo-label echo-label-style-a">たぐ</a></dd>
+                    <dd class="echo-labels-item"><a href="#" class="echo-p-post-tag-label echo-label echo-label-style-a">たぐ</a></dd>
+                    <dd class="echo-labels-item"><a href="#" class="echo-p-post-tag-label echo-label echo-label-style-a">たぐ</a></dd>
+                </dl><!-- /.echo-labels-list -->
+            </nav><!-- /.echo-labels -->
+        </div><!-- /.echo-p-post-category -->
+    </div><!-- /.echo-p-post-footer -->
+
+</section><!-- /.echo-p-post -->
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
