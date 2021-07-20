64e6eba6cd864a2de36e1b08d4e22ca8a5c8b58f
Seiko Kuchida <mail@webbingstudio.com>
2021-07-20 13:22:09 +0900

    ボタンのコンポーネントを作成

---
+++ components/BaseButtons.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="echo-buttons echo-margin-y-lg">
+        <div class="echo-buttons-item">
+            <a href="/" class="echo-button echo-button-style-b echo-button-icon-arrow">ボタン</a>
+        </div><!-- /.echo-buttons-item -->
+    </div><!-- /.echo-p-widget-buttons -->
+</template>
