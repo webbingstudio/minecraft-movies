0b09a1903bf52e13c42b77bfdec7f2ab02c94d99
Seiko Kuchida <mail@webbingstudio.com>
2021-07-21 15:53:04 +0900

    本文にYouTube埋め込みとマークダウン変換を追加

--- components/PostBody.vue
+++ components/PostBody.vue
@@ -1,9 +1,15 @@
 <template>
 <section :id="'post-' + id" :class="postClass" class="echo-p-post echo-container echo-container-xs">
 
+    <div v-if="youtubeId" class="echo-p-post-header echo-margin-bottom-lg">
+        <div class="echo-embed echo-embed-16to9">
+          <iframe width="560" height="315" :src="'https://www.youtube.com/embed/' + youtubeId" :title="title" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div><!-- /.echo-p-post-header -->
+
     <div class="echo-p-post-contents echo-body echo-body-fixed echo-text-sm echo-margin-bottom-lg">
         <!-- eslint-disable-next-line vue/no-v-html -->
-        <div v-html="body"></div>
+        <div v-html="$options.filters.markdown(body)"></div>
     </div><!-- /.echo-p-post-contents -->
 
     <div class="echo-p-post-footer">
@@ -37,6 +43,10 @@ export default {
     title: {
       type: String,
       required: true,
+      default: '',
+    },
+    youtubeId: {
+      type: String,
       default: '',
     },
     date: {
