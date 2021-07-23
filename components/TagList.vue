5ea31ee65cb22e860b1c37c66d9e41a9cd7f160c
Seiko Kuchida <mail@webbingstudio.com>
2021-07-23 20:08:23 +0900

    タグリストのコンポーネントを作成

---
+++ components/TagList.vue
@@ -0,0 +1,37 @@
+<template>
+<dd :class="itemClass" class="echo-labels-item">
+    <a :href="`/tag/` + id + `/`" :class="labelClass" class="echo-p-post-tag-label echo-label">
+        {{ label }}
+    </a>
+</dd>
+</template>
+
+<script>
+export default {
+  props: {
+    itemClass: {
+        type: String,
+        default: '',
+    },
+    labelClass: {
+        type: String,
+        default: 'echo-label-style-a',
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+        default: '',
+    }
+  },
+}
+</script>
+
+<style scoped>
+    .echo-p-post-tag-label:before {
+        content: '#';
+    }
+</style>
