d49442a8e13d44500ebfe7d20fc00845626b3c86
Seiko Kuchida <mail@webbingstudio.com>
2021-07-23 18:43:37 +0900

    詳細ページ本文にmicroCMSの情報を表示してみる

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,11 +1,12 @@
 <template>
     <the-main>
         <base-pagehead
-            :eyecatch-lg="post.eyecatchLg"
-            :eyecatch-lg-url="post.eyecatchLg.url"
-            :eyecatch-sm="post.eyecatchSm"
-            :eyecatch-sm-url="post.eyecatchSm.url"
+            :eyecatch-lg="post.eyecatchLg ? post.eyecatchLg : null"
+            :eyecatch-lg-url="post.eyecatchLg ? post.eyecatchLg.url : null"
+            :eyecatch-sm="post.eyecatchSm ? post.eyecatchSm : null"
+            :eyecatch-sm-url="post.eyecatchSm ? post.eyecatchSm.url : null"
             :title="post.title"
+            :youtube-id="post.youtubeId"
             :date="post.createdAt"
         />
         <post-body
@@ -23,8 +24,8 @@
                     v-for="(post, index) in posts"
                     :id="post.id"
                     :key="index"
-                    :eyecatch-sm="post.eyecatchSm"
-                    :eyecatch-sm-url="post.eyecatchSm.url"
+                    :eyecatch-sm="post.eyecatchSm ? post.eyecatchSm : null"
+                    :eyecatch-sm-url="post.eyecatchSm ? post.eyecatchSm.url : null"
                     :youtube-id="post.youtubeId"
                     :title="post.title"
                     :date="post.createdAt"
@@ -36,31 +37,33 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  async asyncData({ $config, params, error }) {
+    try {
+      const { data } = await axios.get(
+        `${$config.apiUrl}/movies/${params.id}`, {
+          headers: { 'X-API-KEY': $config.apiKey }
+      })
+      return {
+        post: data
+      }
+    } catch (err) {
+      error({
+        errorCode: 404
+      })
+    }
+  },
+  head() {
+    return {
+      title: this.post.title
+    }
+  },
+
+
+
     data() {
         return {
-            post: {
-    "id": "qnfoxodef",
-    "createdAt": "2021-07-14T09:19:53.231Z",
-    "updatedAt": "2021-07-14T09:19:53.231Z",
-    "publishedAt": "2021-07-14T09:19:53.231Z",
-    "revisedAt": "2021-07-14T09:19:53.231Z",
-    "title": "吾輩は人間と同居して彼等を観察すればするほど",
-    "youtubeId": "8nxaZ69ElEc",
-    "eyecatchLg": {
-        "url": "https://picsum.photos/1200/800",
-        "height": 800,
-        "width": 1200
-    },
-    "eyecatchSm": {
-        "url": "https://picsum.photos/640/480",
-        "height": 480,
-        "width": 640
-    },
-    "body": "吾輩の尊敬する筋向の白君などは逢う度毎に人間ほど不人情なものはないと言っておらるる。  白君は先日玉のような子猫を四疋産まれたのである。ところがそこの家の書生が三日目にそいつを裏の池へ持って行って四疋ながら棄てて来たそうだ。\n\n## 白君は涙を流して\n\nその一部始終を話した上、どうしても我等猫族が親子の愛を完くして美しい家族的生活をするには人間と戦ってこれを剿滅せねばならぬといわれた。一々もっともの議論と思う。",
-    "tags": ''
-            },
-
             posts: [
 {
     "id": "qnfoxodef",
